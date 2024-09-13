89dfb8108c9cab0334a28ff510223d4485965f57
Harsh Trada R. <harshtrada51@gmail.com>
2024-09-13 16:10:29 +0530

    Add files via upload

---
+++ Program 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c);
+if(a==b&b==c){
+printf(" Triangle is equal " );
+}
+else{
+printf(" Triangle is not eqaule " );
+}
+return 0;
+}
