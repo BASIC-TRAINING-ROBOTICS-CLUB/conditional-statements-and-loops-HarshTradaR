e43d4e0ccbae511b54352205356ba4c78bfaeea9
Harsh Trada R. <harshtrada51@gmail.com>
2024-09-20 11:53:54 +0530

    Update Task 2 (1).c

--- Task 2 (1).c
+++ Task 2 (1).c
@@ -1,42 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 
-#define GRAVITY 9.81
+#define Gravity 9.81
 
-double degrees_to_radians(double degrees) {
-    return degrees * (M_PI / 180.0);
-}
+void projectile_motion(double initial_velocity, double angle, double *time_of_flight, double *max_height, double *range) {
+ 
+    double angle_radian = angle * (3.14 / 180.0);
+    
+ 
+    *time_of_flight = (2 * initial_velocity * sin(angle_radian)) / Gravity;
+    
+    
+    *max_height = (initial_velocity * initial_velocity * sin(angle_radian) * sin(angle_radian)) / (2 * Gravity);
+    
 
-double calculate_time_of_flight(double initial_velocity, double angle_in_degrees) {
-    double angle_in_radians = degrees_to_radians(angle_in_degrees);
-    return (2 * initial_velocity * sin(angle_in_radians)) / GRAVITY;
-}
-
-double calculate_max_height(double initial_velocity, double angle_in_degrees) {
-    double angle_in_radians = degrees_to_radians(angle_in_degrees);
-    return (pow(initial_velocity, 2) * pow(sin(angle_in_radians), 2)) / (2 * GRAVITY);
-}
-
-double calculate_range(double initial_velocity, double angle_in_degrees) {
-    double angle_in_radians = degrees_to_radians(angle_in_degrees);
-    return (pow(initial_velocity, 2) * sin(2 * angle_in_radians)) / GRAVITY;
+    *range = (initial_velocity * initial_velocity * sin(2 * angle_radian)) / Gravity;
 }
 
 int main() {
-    double velocity, angle;
-
-    printf("Enter the initial velocity (m/s): ");
-    scanf("%lf", &velocity);
-    printf("Enter the angle of projection (degrees): ");
-    scanf("%lf", &angle);
-
-    double time_of_flight = calculate_time_of_flight(velocity, angle);
-    double max_height = calculate_max_height(velocity, angle);
-    double range = calculate_range(velocity, angle);
-
-    printf("Time of Flight: %.2f seconds\n", time_of_flight);
-    printf("Maximum Height: %.2f meters\n", max_height);
-    printf("Range: %.2f meters\n", range);
-
-    return 0;
+    double initial_velocity = 510;
+    double angle = 72.0;   
+    double time_of_flight, max_height, range;
+    
+    projectile_motion(initial_velocity, angle, &time_of_flight, &max_height, &range);
+    
+    printf("Time of Flight: %.2f s\n", time_of_flight);
+    printf("Maximum Height: %.2f m\n", max_height);
+    printf("Range: %.2f m\n", range);
+    
+return 0;
 }
